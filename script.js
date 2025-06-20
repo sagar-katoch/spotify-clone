cbaffb136808ced7c022ffb7215599d1e6abcfca
sagar katoch <katochsagar2@gmail.com>
2025-06-21 03:31:32 +0530

    hi

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("javascript is running")
+
+
+
+
+async function main(){
+
+let a=await fetch("http://127.0.0.1:3000/songs/")
+let response= await a.text()
+
+console.log(response)
+let div= document.createElement("div")
+div.innerHTML=response
+let as= div.getElementsByTagName("a")
+console.log(as)
+let songs=[]
+
+for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if(element.href.endsWith(".mp3")){
+        songs.push(element.href)
+    }
+    
+}
+
+
+
+}
+main()
