eb87567f21f824f7fa6aed364bc835118b9fb1fb
sagar katoch <katochsagar2@gmail.com>
2025-06-21 03:54:56 +0530

    hi

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ console.log("javascript is running")
 
 
 
-async function main(){
+async function getsongs(){
 
 let a=await fetch("http://127.0.0.1:3000/songs/")
 let response= await a.text()
 
-console.log(response)
+
 let div= document.createElement("div")
 div.innerHTML=response
 let as= div.getElementsByTagName("a")
-console.log(as)
+
 let songs=[]
 
 for (let index = 0; index < as.length; index++) {
@@ -23,7 +23,18 @@ for (let index = 0; index < as.length; index++) {
     
 }
 
-
+return songs
 
 }
+
+async function main(){
+// get list of all songs
+let songs=await getsongs()
+console.log(songs)
+
+//playing our first song
+var audio = new Audio(songs[0]);
+audio.play();
+   
+}    
 main()
